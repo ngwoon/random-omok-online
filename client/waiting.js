9c57711cc14e35e14b9929c0b109b05f503b5f22
ngwoon <todory2002@gmail.com>
2020-10-04 15:03:58 +0900

    오목판 색 변경

--- client/waiting.js
+++ client/waiting.js
@@ -26,17 +26,17 @@ function init() {
 
             // 사용자가 의도적으로 게임을 끈 상태랑, 매칭이 되어서 ready 페이지로 넘어갈 때랑 구분하기 위해
             // ready 상태 진입 시 onbeforeunload 이벤트를 침묵시킨다.
-            window.onbeforeunload = function(event) {
-                event.preventDefault();
-            }
+            window.removeEventListener("onbeforeunload", beforeUnLoadListener, true);
             location.href = SERVER+"/ready";
         }
     });
 
-    window.onbeforeunload = function(event) {
-        event.preventDefault();
-        fetch(SERVER+"/out");
-    }
+    window.addEventListener = ("onbeforeunload", beforeUnLoadListener, true);
+}
+
+function beforeUnLoadListener(event) {
+    event.preventDefault();
+    fetch(SERVER+"/out");
 }
 
 init();
